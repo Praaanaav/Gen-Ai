11f2d9b041b5b8e3e0993035ad2fbc989b8b081a
Pranav <kumarpranav15404@gmail.com>
2026-08-05 00:38:14 +0530

    Basic encoding and decoding (vid-02)

--- notebook/01.py
+++ notebook/01.py
@@ -4,8 +4,10 @@ tokenizer = tiktoken.encoding_for_model("gpt-4");
 # print(tokenizer)
 
 text = "Hey How Are You"
+print (text)
 tokenId = tokenizer.encode(text)
 print(tokenId)
 
-result = tokenizer.decode( [19182, 2650, 8886, 1472])
+# result_text = "I'm good what about you"
+result = tokenizer.decode( [40, 2846, 1695, 1148, 922, 499])
 print(result)
